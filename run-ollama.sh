37b700468b19bb9ac8cc7f6d4e7e988f30f3ec6c
Edward <edwardduong71311@gmail.com>
2024-10-21 21:57:24 -0500

    fix(RHA-2-1): add permission

--- run-ollama.sh
+++ run-ollama.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+echo "Starting Ollama server..."
 ollama serve &
+
+echo "Waiting for Ollama server to be active..."
+while [ "$(ollama list | grep 'NAME')" == "" ]; do
+  sleep 1
+done
+
+
+echo "Pulling embedding model..."
 ollama pull mxbai-embed-large
+
+echo "Pulling llama 3.2 model..."
 ollama pull llama3.2
